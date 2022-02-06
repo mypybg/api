393bc3c34676b6a5a392837f5661b1cfdef17733
mypybg <99121641+mypybg@users.noreply.github.com>
2022-02-06 14:02:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-import .Telegram
+import requests, re
+from flask import Flask , request ,jsonify
+app=Flask(__name__)
+@app.route("/")
+		
+def check():
+	user = request.args.get("user")
+	r = requests.get(f"https://t.me/{user}").text
+	
+	find = re.findall('<div class="tgme_page_title"><span dir="auto">(.*?)</span></div>', r)
+	
+	try:
+		if find[0] != None:
+			ok = jsonify(ok=False, status="UnAvailable", user=user)
+	except:
+			ok = jsonify(ok= True,status="Available",user=user)
+			
+	return ok			
+app.run()
